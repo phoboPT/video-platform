7a5b123cc02426514b588b96d438ea70400c3931
Ruben-Linux <rubenmorim17@gmail.com>
2019-02-18 15:15:18 +0000

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
-require('dotenv').config({ path: 'variables.env' });
-const createServer = require('./createServer');
-const db = require('./db');
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
 
 const server = createServer();
 
@@ -11,8 +11,8 @@ server.start(
   {
     cors: {
       credentials: true,
-      origin: process.env.FRONTEND_URL,
-    },
+      origin: process.env.FRONTEND_URL
+    }
   },
   deets => {
     console.log(`Server is now running on port http:/localhost:${deets.port}`);
