5847c71cd1f437b0a0c71856dcb78b17b7f59826
Ruben-Linux <rubenmorim17@gmail.com>
2019-02-19 17:42:44 +0000

    feat: comentarios crud

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,7 +3,10 @@ const { forwardTo } = require("prisma-binding");
 const Query = {
   videos: forwardTo("db"),
   categorys: forwardTo("db"),
-  video: forwardTo("db")
+  comvideos: forwardTo("db"),
+  video: forwardTo("db"),
+  category: forwardTo("db"),
+  comvideo: forwardTo("db")
 
   // async videos(parent, args, ctx, info) {
   //   //ctx.db.query-> vai ao contexto->db -> query no schema do prisma e retira de la os videos
