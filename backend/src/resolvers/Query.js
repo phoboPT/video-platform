b0f0039c501920f456eb716f7d54f9b7e15925e0
Ruben-Linux <rubenmorim17@gmail.com>
2019-02-20 17:28:13 +0000

    update

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -7,11 +7,6 @@ const Query = {
   users: forwardTo("db"),
   video: forwardTo("db"),
   category: forwardTo("db"),
-<<<<<<< HEAD
-=======
-  comVideo: forwardTo("db"),
-
->>>>>>> e55a451b5702f499b618649b4b942c1f6f564ba3
   me(parent, args, ctx, info) {
     const { userId } = ctx.request;
     //checkar se tem um current ID
@@ -44,13 +39,8 @@ const Query = {
       info
     );
   },
-<<<<<<< HEAD
   comsVideo(parent, args, ctx, info) {
     console.log(args);
-=======
-  comVideos(parent, args, ctx, info) {
-    const { userId } = ctx.request;
->>>>>>> e55a451b5702f499b618649b4b942c1f6f564ba3
     //Ver se esta logado
     if (!userId) {
       throw new Error("you must be signed in!");
@@ -59,11 +49,7 @@ const Query = {
     return ctx.db.query.comVideo(
       {
         where: {
-<<<<<<< HEAD
           video: { id: args.id }
-=======
-          user: { id: userId }
->>>>>>> e55a451b5702f499b618649b4b942c1f6f564ba3
         }
       },
       info
