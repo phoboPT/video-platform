27d1bb102b93ab45995ea029589a19d7bde4b17b
Ruben-Linux <rubenmorim17@gmail.com>
2019-02-26 14:36:17 +0000

    feat: searchbar 1 parte

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Nav from "./Nav";
 import Router from "next/router";
 import NProgress from "nprogress";
+import Search from "./Search";
 
 Router.onRouteChangeStart = () => {
   NProgress.start();
@@ -65,7 +66,7 @@ const Header = () => (
       <Nav />
     </div>
     <div className="sub-bar">
-      <p>Search</p>
+      <Search />
     </div>
     <div>Cart</div>
   </StyledHeader>
--- frontend/components/Search.js
+++ frontend/components/Search.js
@@ -1,6 +1,74 @@
-import React from "react";
+import React, { Component } from "react";
 import Downshift from "downshift";
 import Router from "next/router";
-import { ApolloConsumer } from "react-adopt";
+import { ApolloConsumer } from "react-apollo";
 import gql from "graphql-tag";
 import debounce from "lodash.debounce";
+import { DropDown, DropDownItem, SearchStyles } from "./styles/DropDown";
+
+const SEARCH_VIDEOS_QUERY = gql`
+  query SEARCH_VIDEOS_QUERY($searchTerm: String!) {
+    videosUser(
+      where: {
+        OR: [
+          { title_contains: $searchTerm }
+          { description_contains: $searchTerm }
+        ]
+      }
+    ) {
+      id
+      title
+      description
+    }
+  }
+`;
+
+export class AutoComplete extends Component {
+  state = {
+    videos: [],
+    loading: false
+  };
+  onChange = debounce(async (e, client) => {
+    console.log("Searching results...");
+    //turn loading on
+    this.setState({ loading: true });
+    const res = await client.query({
+      query: SEARCH_VIDEOS_QUERY,
+      variables: { searchTerm: e.target.value }
+    });
+    this.setState({
+      videos: res.data.videosUser,
+      loading: false
+    });
+  }, 400);
+  render() {
+    return (
+      <SearchStyles>
+        <div>
+          <ApolloConsumer>
+            {client => (
+              <input
+                type="search"
+                onChange={e => {
+                  e.persist();
+                  this.onChange(e, client);
+                }}
+              />
+            )}
+          </ApolloConsumer>
+
+          <DropDown>
+            {this.state.videos.map(video => (
+              <DropDownItem key={video.id}>
+                <p>nome {video.title}</p>
+                <p>descricao {video.description}</p>
+              </DropDownItem>
+            ))}
+          </DropDown>
+        </div>
+      </SearchStyles>
+    );
+  }
+}
+
+export default AutoComplete;
