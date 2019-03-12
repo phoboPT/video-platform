a2b41ba6a8756451b75593c28e5033313b8bd770
Ruben-Linux <rubenmorim17@gmail.com>
2019-03-12 11:37:39 +0000

    feat : comments style

--- frontend/pages/updateComment.js
+++
@@ -1,9 +0,0 @@
-import UpdateComCourse from "../components/Courses/Comments/UpdateComment";
-
-const Update = ({ query }) => (
-  <div>
-    <UpdateComCourse id={query.id} />
-  </div>
-);
-
-export default Update;
