b3b834798b3ad56ed4f217b98896efe01153e724
rubenmorim17 <rubenmorim17@gmail.com>
2019-06-27 14:29:04 +0100

    fix: error page

--- frontend/pages/_error.js
+++ frontend/pages/_error.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
+const Style = styled.div`
+  width: 100%;
+  text-align: center;
+  img {
+    margin-top: 8rem;
+  }
+`;
 export class _error extends Component {
   render() {
     return (
-      <div>
-        <p>Page Not Found Sorry</p>
-      </div>
+      <Style>
+        <img alt="Page Not Found" src="../static/error.webp" />
+        <p> Sorry, this page could not be found !</p>
+      </Style>
     );
   }
 }
